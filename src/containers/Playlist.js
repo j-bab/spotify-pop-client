bce05b38f1c00384d0df24ba3da93fdcd9c53447
james <james@scubatribe.com>
2021-02-19 10:26:55 +0100

    refactor playlist to reduce complexity

---
+++ src/containers/Playlist.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from "react-bootstrap/esm/Card";
+
+export const Playlist = (props) => {
+    const {tracks, images, name, owner} = props.item;
+    return <Card>
+        {images && images.length > 0 && <Card.Img variant="top" src={images[0].url}/>}
+        <Card.Body>
+            <Card.Title>{name}</Card.Title>
+            <Card.Text>
+                {owner && owner.display_name && <p>By <strong>{owner.display_name}</strong></p>}
+            </Card.Text>
+        </Card.Body>
+        <Card.Header as="h5">{tracks.total} Tracks</Card.Header>
+    </Card>;
+};
