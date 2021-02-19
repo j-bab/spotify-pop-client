f286d47768f2630a7ea1ff234e6a2894edf296fa
james <james@scubatribe.com>
2021-02-19 10:27:22 +0100

    refactor artist to reduce complexity

--- src/containers/Artist.js
+++ src/containers/Artist.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Card from "react-bootstrap/esm/Card";
-import CardColumns from "react-bootstrap/esm/CardColumns";
 
-function Artist(props) {
+export const Artist = (props) => {
     const {followers, images, name, genres, popularity} = props.item;
 
     return <Card>
         {images && images.length > 0 && <Card.Img variant="top" src={images[0].url}/>}
         <Card.Body>
-            <Card.Title><span>{name}</span></Card.Title>
+            <Card.Title>{name}</Card.Title>
             <Card.Text>{genres && genres.length > 0 && `Genres: ${genres.join(', ')}`}
                 <br/>
                 <em>{followers.total} Followers</em>
@@ -16,14 +15,5 @@ function Artist(props) {
         </Card.Body>
         {popularity && popularity > 70 && <Card.Header as="h5">Popularity: {popularity}</Card.Header>}
     </Card>;
-}
+};
 
-
-export function Artists(props) {
-    let {items} = props;
-    return <CardColumns>
-        {items.map(function (item, index) {
-            return <Artist key={index} item={item}/>;
-        })}
-    </CardColumns>
-}
